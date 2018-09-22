6fedf0e66572884de24319816e70274deb79f76d
Administrator <745499691@qq.com>
2018-09-22 22:31:41 +0800

    hujian

--- manage.py
+++ manage.py
@@ -7,7 +7,9 @@ from app.models import db
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 import os
+import re
 from config import BASEDIR
+from app.models import User
 
 app.config['SECRET_KEY'] = 'odinhj_123456'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS '] = False
@@ -20,6 +22,20 @@ app.register_blueprint(student_blueprint, url_prefix="/student")
 Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def create_user():
+    username = input("uername:")
+    password = input('password:')
+    password2 = input('two password:')
+    email = input('email:')
+    phone = input('phone:')
+    if password == password2 and re.match(r'[0-9a-zA-Z_]{0,19}@qq.com', email) and phone:
+        user = User(username=username, password=password, email=email, phone=phone)
+        db.session.add(user)
+        db.session.commit()
+        print("注册成功！！！！")
 
 
 if __name__ == '__main__':
